d86a101864aefd668a9bf50b314389e390bc0715
Fabio <dasilvadeoliveirafabio@email.com>
2026-07-11 19:23:39 -0300

    teste sem o prisma 6

--- controllers/usuarioController.ts
+++ controllers/usuarioController.ts
@@ -1,5 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+const prisma = new PrismaClient()
+export const listarUsuario = async(req:Request,res:Response)=>{
+    const usuario = await prisma.usuario.findMany()
 
-export const listarUsuario = (req:Request,res:Response)=>{
-    res.json("probrama rodando...")
+    res.json(usuario)
 }
