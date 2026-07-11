fe000f0157bb8e75e8f0115a0e308f20e295dffb
Fabio <dasilvadeoliveirafabio@email.com>
2026-07-11 19:03:36 -0300

    teste sem o prisma

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import { listarUsuario } from "./controllers/usuarioController";
+const app =  express()
+
+app.use(cors())
+app.use(express.json())
+app.get("/", listarUsuario)
+app.listen(4000,()=>console.log("rodando na porta 4000"))
