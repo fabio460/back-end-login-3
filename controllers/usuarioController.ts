25b063436c6ab2787e52e3fc872b1bb8e234cd1c
Fabio <dasilvadeoliveirafabio@email.com>
2026-07-12 11:04:18 -0300

    rotas criadas

--- controllers/usuarioController.ts
+++ controllers/usuarioController.ts
@@ -1,8 +1,76 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const chaveSecreta = "chavetoken"
 const prisma = new PrismaClient()
+
 export const listarUsuario = async(req:Request,res:Response)=>{
     const usuario = await prisma.usuario.findMany()
-
     res.json(usuario)
+}
+
+export const cadastrarUsuario = async (req:Request, res:Response)=>{
+    const {nome, email, senha} = req.body
+    if (!nome && !email && !senha) {
+        return res.status(400).json("insira os dados completos!")
+    }
+    const usuario = await prisma.usuario.findUnique({
+        where:{
+            email
+        }
+    })
+    if (usuario) {
+        return res.status(401).json("Este usuário já existe!")
+    }
+    const salt = await bcrypt.genSalt(10)
+    const senhaCriptografada = await bcrypt.hash(senha,salt)
+    try {
+        await prisma.usuario.create({
+            data:{
+                email,
+                nome,
+                senha:senhaCriptografada
+            }
+        })
+       return res.status(200).json("Usuario cadastrado com succeso!")
+    } catch (error) {
+       return res.status(401).json("Falha ao cadastrar usuário!")
+    }
+}
+
+export const logarUsuario =  async (req:Request, res:Response)=>{
+   const {email, senha} = req.body
+
+   if (!email && !senha) {
+      res.status(401).json("Insira um email e uma senha")
+   }
+   const usuario = await prisma.usuario.findUnique({where:email})
+   try {    
+       const senhaValida =await bcrypt.compare(senha, usuario?.senha as string)
+       if (senhaValida) {
+          const token = jwt.sign({usuario},chaveSecreta,{expiresIn:"1d"})
+          return res.status(200).json({
+            id:usuario?.id,
+            nome:usuario?.nome, 
+            email:usuario?.email,
+            token
+        })
+       }
+   } catch (error) {
+      return res.status(400).json("Email ou senha in válidos!")
+   }
+}
+
+export const autenticarUsuario =  async (req:Request, res:Response)=>{
+    try {
+       const token = req.headers.authorization?.split(" ")[1] as string
+       jwt.verify(token,chaveSecreta)
+       const usuario = jwt.decode(token)
+       res.status(200).json({
+         usuario
+       })
+   } catch (error) {
+      res.status(400).json("Usuário não esta logado!")
+   }
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { listarUsuario } from "./controllers/usuarioController.js";
+import { autenticarUsuario, cadastrarUsuario, listarUsuario, logarUsuario } from "./controllers/usuarioController.js";
 const app =  express()
 
 app.use(cors())
 app.use(express.json())
 app.get("/", listarUsuario)
+app.post("/cadastrar",cadastrarUsuario)
+app.post("/logar",logarUsuario)
+app.post("/autenticar",autenticarUsuario)
 
 app.listen(4000,()=>console.log("rodando na porta 4000"))
